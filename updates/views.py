70d07237844ab26683ff51f6c05ca6417ab49732
Devendra Dewangan <devendra.dewangan129@gmail.com>
2020-04-21 20:04:16 +0530

    HttpResponse with JSON data

--- updates/views.py
+++ updates/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import json
 
 # Create your views here.
 
@@ -7,6 +8,7 @@ from django.http import JsonResponse
 def update_model_detail_view(request):
     '''
     URI - for a REST API
+    GET - Retrieve
     '''
     data = {
         'count': 1000,
@@ -14,3 +16,17 @@ def update_model_detail_view(request):
     }
 
     return JsonResponse(data)
+
+
+def json_example_view(request):
+    '''
+    URI - for a REST API
+    GET - Retrieve
+    '''
+    data = {
+        'count': 1000,
+        'content': 'Some new content'
+    }
+    json_data = json.dumps(data)
+
+    return HttpResponse(json_data, content_type='application/json')
