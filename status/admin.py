8ed63d9abefd2ae048e189077d5da6eeaeb6804c
Devendra Dewangan <devendra.dewangan129@gmail.com>
2020-04-24 13:43:01 +0530

    Model Form for Validation

--- status/admin.py
+++ status/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+
+from .forms import StatusForm
 from .models import Status
+
 # Register your models here.
-admin.site.register(Status)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', '__str__', 'image']
+    form = StatusForm
+
+    # class Meta:
+    #     model = Status
+
+
+admin.site.register(Status, StatusAdmin)
--- status/models.py
+++ status/models.py
@@ -17,7 +17,8 @@ class StatusManager(models.Manager):
 
 
 class Status(models.Model):  # Fb status, insta post, tweet, linkedin post
-    user = models.ForeignKey
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     content = models.TextField(null=True, blank=True)
     image = models.ImageField(
         upload_to=upload_update_image, null=True, blank=True)
@@ -28,3 +29,7 @@ class Status(models.Model):  # Fb status, insta post, tweet, linkedin post
 
     def __str__(self):
         return str(self.content)[:50]
+
+    class Meta:
+        verbose_name = "Status post"
+        verbose_name_plural = "Status posts"
