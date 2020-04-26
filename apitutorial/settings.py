c02716e2d1ed290f8893b9ac33dda0939ef93ae9
Devendra Dewangan <devendra.dewangan129@gmail.com>
2020-04-26 13:15:16 +0530

    16 - Uploading  & Handling Images

--- apitutorial/settings.py
+++ apitutorial/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(
+    BASE_DIR, 'static-server', 'media-root')
+MEDIA_URL = '/media/'
